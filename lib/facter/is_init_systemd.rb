e49932423365eee4ddd5d8cd82f508cf879404c4
Alexander Ursu <alexander.ursu@gmail.com>
2018-02-09 13:45:33 +0100

    Added fact is_init_systemd - returns true if OS (only RedHat based) is running with systemd

---
+++ lib/facter/is_init_systemd.rb
@@ -0,0 +1,24 @@
+# Fact: is_init_systemd
+#
+# Purpose: check if OS init system is systemd
+#
+# Resolution:
+#   Supports only RedHat (initially). Check OS type and version and return
+#   Boolean result
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
+Facter.add(:is_init_systemd) do
+  setcode do
+    osfamily = Facter.value(:osfamily)
+    osname = Facter.value(:operatingsystem)
+    osmaj = Facter.value(:operatingsystemmajrelease)
+    if osname.casecmp("Fedora") == 0 or (osfamily.casecmp("RedHat") and osmaj.to_i >= 7)
+        return true
+    end
+    return false
+  end
+end
