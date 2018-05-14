e917d07f98c22c3f37de08d790834876389bcdab
Alexander Ursu <alexander.ursu@gmail.com>
2018-05-14 02:03:38 +0200

    Added Ubuntu system support

--- lib/facter/is_init_systemd.rb
+++ lib/facter/is_init_systemd.rb
@@ -11,17 +11,14 @@
 #
 # Notes:
 #   None
-status = false
-
 osfamily = Facter.value(:osfamily).to_s
 osname = Facter.value(:operatingsystem).to_s
 osmaj = Facter.value(:operatingsystemmajrelease).to_i
 osrel = Facter.value(:operatingsystemrelease).to_s
 
-if osname.casecmp('Fedora').zero? || (osfamily.casecmp('RedHat').zero? && osmaj >= 7) ||
-  (osname.casecmp('Ubuntu').zero? && (Puppet::Util::Package.versioncmp(osrel, '15.04') >= 0))
-  status = true
-end
+status = osname.casecmp('Fedora').zero? ||
+         (osfamily.casecmp('RedHat').zero? && osmaj >= 7) ||
+         (osname.casecmp('Ubuntu').zero? && Puppet::Util::Package.versioncmp(osrel, '15.04') >= 0)
 
 Facter.add(:is_init_systemd) do
   setcode do
