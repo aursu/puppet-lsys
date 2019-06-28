c6580cdb45b59f9e4cb881aeca7c6da50411a711
Alexander Ursu <alexander.ursu@gmail.com>
2019-06-28 18:09:06 +0200

    Added pxe grub setup

---
+++ manifests/pxe/grub.pp
@@ -0,0 +1,39 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include lsys::pxe::grub
+class lsys::pxe::grub {
+
+  # GRUB2 Modules installation
+  package {
+    default:
+      ensure  => 'present',
+    ;
+    'grub2-tools-extra':
+    ;
+    [ 'grub2-efi-ia32-modules', 'grub2-pc-modules', 'grub2-efi-x64-modules' ]:
+      require => Package['grub2-tools-extra'],
+    ;
+  }
+
+  # GRUB2 TFTP data
+  exec { 'grub2-mknetdir --net-directory=/var/lib/tftpboot --subdir=boot/grub -d /usr/lib/grub/x86_64-efi':
+    path    => '/usr/bin:/bin',
+    creates => '/var/lib/tftpboot/boot/grub/x86_64-efi/core.efi',
+    require => Package['grub2-efi-x64-modules'],
+  }
+
+  exec { 'grub2-mknetdir --net-directory=/var/lib/tftpboot --subdir=boot/grub -d /usr/lib/grub/i386-pc':
+    path    => '/usr/bin:/bin',
+    creates => '/var/lib/tftpboot/boot/grub/i386-pc/core.0',
+    require => Package['grub2-pc-modules'],
+  }
+
+  exec { 'grub2-mknetdir --net-directory=/var/lib/tftpboot --subdir=boot/grub -d /usr/lib/grub/i386-efi':
+    path    => '/usr/bin:/bin',
+    creates => '/var/lib/tftpboot/boot/grub/i386-efi/core.efi',
+    require => Package['grub2-pc-modules'],
+  }
+}
