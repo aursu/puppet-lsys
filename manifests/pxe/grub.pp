75ff660c5c7e307dd2bbc68781a0934903ee57f2
Alexander Ursu <alexander.ursu@gmail.com>
2019-07-02 00:28:08 +0200

    Added GRUB configuration file

--- manifests/pxe/grub.pp
+++ manifests/pxe/grub.pp
@@ -29,11 +29,42 @@ class lsys::pxe::grub {
     path    => '/usr/bin:/bin',
     creates => '/var/lib/tftpboot/boot/grub/i386-pc/core.0',
     require => Package['grub2-pc-modules'],
+    before  => File['/var/lib/tftpboot/boot/grub/grub.cfg']
   }
 
   exec { 'grub2-mknetdir --net-directory=/var/lib/tftpboot --subdir=boot/grub -d /usr/lib/grub/i386-efi':
     path    => '/usr/bin:/bin',
     creates => '/var/lib/tftpboot/boot/grub/i386-efi/core.efi',
     require => Package['grub2-pc-modules'],
+  }
+
+  # GRUB configuration files
+  # MAC: 00:50:56:8e:5b:30
+  # IP: 10.55.156.40 (hex: 0A.37.9C.28)
+  # /boot/grub/i386-pc/grub.cfg-01-00-50-56-8e-5b-30
+  # /boot/grub/i386-pc/grub.cfg-01-00-50-56-8e-5b-30
+  # /boot/grub/i386-pc/grub.cfg-0A379C28
+  # /boot/grub/i386-pc/grub.cfg-0A379C28
+  # /boot/grub/i386-pc/grub.cfg-0A379C2
+  # /boot/grub/i386-pc/grub.cfg-0A379C2
+  # /boot/grub/i386-pc/grub.cfg-0A379C
+  # /boot/grub/i386-pc/grub.cfg-0A379C
+  # /boot/grub/i386-pc/grub.cfg-0A379
+  # /boot/grub/i386-pc/grub.cfg-0A379
+  # /boot/grub/i386-pc/grub.cfg-0A37
+  # /boot/grub/i386-pc/grub.cfg-0A37
+  # /boot/grub/i386-pc/grub.cfg-0A3
+  # /boot/grub/i386-pc/grub.cfg-0A3
+  # /boot/grub/i386-pc/grub.cfg-0A
+  # /boot/grub/i386-pc/grub.cfg-0A
+  # /boot/grub/i386-pc/grub.cfg-0
+  # /boot/grub/i386-pc/grub.cfg-0
+  # /boot/grub/i386-pc/grub.cfg
+  #
+  # cat boot/grub/i386-pc/grub.cfg
+  # source boot/grub/grub.cfg
+  file { '/var/lib/tftpboot/boot/grub/grub.cfg':
+    ensure  => file,
+    content => file('lsys/pxe/grub.cfg'),
   }
 }
