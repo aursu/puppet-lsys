bba5a252fca01809c8657e18f38952cfa97528a1
Alexander Ursu <alexander.ursu@gmail.com>
2019-07-30 23:09:03 +0200

    Added VMWare vSphere SDK installation rules

---
+++ manifests/vmware/sdk.pp
@@ -0,0 +1,19 @@
+# VMWare vSphere Perl SDK installation
+#
+# @summary VMWare vSphere Perl SDK installation
+# @example
+#   include lsys::vmware::sdk
+class lsys::vmware::sdk (
+  String $package_ensure = '6.7.0-8156551.el7.1',
+)
+{
+  include lsys::repo::bintray
+
+  package { 'VMware-vSphere-Perl-SDK':
+    ensure          => $package_ensure,
+    install_options => [
+      { '--enablerepo' => 'bintray-custom' }
+    ],
+    require         => Yumrepo['bintray-custom'],
+  }
+}
