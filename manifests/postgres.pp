3435ed3437fe76ff595736533086daa31482187b
Alexander Ursu <alexander.ursu@gmail.com>
2020-09-08 01:53:11 +0200

    Added comment for param ip_mask_allow_all_users

--- manifests/postgres.pp
+++ manifests/postgres.pp
@@ -4,6 +4,15 @@
 #
 # @example
 #   include lsys::postgres
+#
+# @param ip_mask_allow_all_users
+#   Overrides PostgreSQL defaults for remote connections. By default, PostgreSQL
+#   does not allow database user accounts to connect via TCP from remote
+#   machines. If you'd like to allow this, you can override this setting. Set to
+#   '0.0.0.0/0' to allow database users to connect from any remote machine, or
+#   '192.168.0.0/1' to allow connections from any machine on your local '192.168'
+#   subnet. Default value: '127.0.0.1/32'.
+#
 class lsys::postgres (
     Boolean $manage_package_repo        = true,
     # https://www.postgresql.org/docs/11/pgupgrade.html
