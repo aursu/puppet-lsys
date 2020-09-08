39b1c90ab284bda9c3acb489f515e06788a81635
Alexander Ursu <alexander.ursu@gmail.com>
2020-09-08 12:42:31 +0200

    Added ability to manage repo SSL verification for postgres

--- manifests/postgres.pp
+++ manifests/postgres.pp
@@ -14,27 +14,35 @@
 #   subnet. Default value: '127.0.0.1/32'.
 #
 class lsys::postgres (
-    Boolean $manage_package_repo        = true,
-    # https://www.postgresql.org/docs/11/pgupgrade.html
-    Pattern[/([89]|1[0-2])(\.[0-9]+)+/]
-            $package_version            = '12.4',
-    String  $ip_mask_allow_all_users    = '0.0.0.0/0',
-    String  $listen_addresses           = 'localhost',
-    Variant[Integer, Pattern[/^[0-9]+$/]]
-            $database_port              = 5432,
+  Boolean $manage_package_repo        = true,
+  # https://www.postgresql.org/docs/11/pgupgrade.html
+  Pattern[/([89]|1[0-2])(\.[0-9]+)+/]
+          $package_version            = '12.4',
+  String  $ip_mask_allow_all_users    = '0.0.0.0/0',
+  String  $listen_addresses           = 'localhost',
+  Variant[Integer, Pattern[/^[0-9]+$/]]
+          $database_port              = 5432,
+  Optional[Integer[0,1]]
+          $repo_sslverify             = undef,
 ){
-    if $manage_package_repo {
-        class { 'postgresql::globals':
-            manage_package_repo => $manage_package_repo,
-            version             => $package_version,
-        }
+  if $manage_package_repo {
+    class { 'postgresql::globals':
+      manage_package_repo => $manage_package_repo,
+      version             => $package_version,
     }
 
-    class { 'postgresql::server':
-        ip_mask_allow_all_users => $ip_mask_allow_all_users,
-        listen_addresses        => $listen_addresses,
-        port                    => $database_port + 0,
+    if $repo_sslverify {
+      Yumrepo <| title == 'yum.postgresql.org' |> {
+        sslverify => $repo_sslverify,
+      }
     }
+  }
 
-    class { 'postgresql::server::contrib': }
+  class { 'postgresql::server':
+    ip_mask_allow_all_users => $ip_mask_allow_all_users,
+    listen_addresses        => $listen_addresses,
+    port                    => $database_port + 0,
+  }
+
+  class { 'postgresql::server::contrib': }
 }
