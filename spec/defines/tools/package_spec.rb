7a74877cabc4ecc96c25368bcc250840903547f3
Alexander Ursu <alexander.ursu@gmail.com>
2021-01-28 00:53:57 +0100

    Corrected rubocop errors

--- spec/defines/tools/package_spec.rb
+++ spec/defines/tools/package_spec.rb
@@ -16,23 +16,27 @@ describe 'lsys::tools::package' do
 
       it { is_expected.not_to contain_package('sudo') }
 
-      context "check boolean ensure => false" do
+      context 'check boolean ensure => false' do
         let(:params) { { ensure: false } }
+
         it { is_expected.not_to contain_package('sudo') }
       end
 
-      context "check boolean ensure => true" do
+      context 'check boolean ensure => true' do
         let(:params) { { ensure: true } }
+
         it { is_expected.to contain_package('sudo').with_ensure('installed') }
       end
 
-      context "check string ensure => latest" do
+      context 'check string ensure => latest' do
         let(:params) { { ensure: 'latest' } }
+
         it { is_expected.to contain_package('sudo').with_ensure('latest') }
       end
 
-      context "check string ensure => numeric" do
+      context 'check string ensure => numeric' do
         let(:params) { { ensure: '1.8.23-10.el7_9.1' } }
+
         it { is_expected.to contain_package('sudo').with_ensure('1.8.23-10.el7_9.1') }
       end
     end
