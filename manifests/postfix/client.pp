a788434749eabfbf72ebf2792a9cc43b20148032
Alexander Ursu <alexander.ursu@gmail.com>
2021-04-02 02:03:48 +0200

    Added relayhost into lsys::postfix::client

--- manifests/postfix/client.pp
+++ manifests/postfix/client.pp
@@ -2,11 +2,20 @@
 #
 # Postfix mail client installation
 #
+# @param relayhost
+#  The next-hop destination(s) for non-local mail; overrides non-local domains in recipient addresses
+#  http://www.postfix.org/postconf.5.html#relayhost
+#
 # @example
 #   include lsys::postfix::client
-class lsys::postfix::client {
+class lsys::postfix::client (
+  Optional[Variant[Stdlib::Fqdn, Stdlib::IP::Address]]
+          $relayhost = undef,
+)
+{
   class { 'postfix':
     manage_mailx   => false,
     manage_aliases => false,
+    relayhost      => $relayhost,
   }
 }
