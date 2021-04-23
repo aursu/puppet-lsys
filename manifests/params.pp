fbda382d9e87ed4027fc06509da20fe2f25b7227
Alexander Ursu <alexander.ursu@gmail.com>
2021-04-23 11:46:34 +0200

    Nginx default versions upgrade

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,9 +15,9 @@ class lsys::params {
   $nginx_user_home       = '/var/lib/nginx'
 
   $nginx_version = $facts['os']['release']['major'] ? {
-    '6'     => '1.19.0-1.el6.ngx',
-    '8'     => '1.19.0-1.el8.ngx',
-    default => '1.19.0-1.el7.ngx',
+    '6'     => '1.19.5-1.el6.ngx',
+    '8'     => '1.19.9-1.el8.ngx',
+    default => '1.19.9-1.el7.ngx',
   }
 
   $nginx_conf_dir        = $nginx::params::conf_dir
