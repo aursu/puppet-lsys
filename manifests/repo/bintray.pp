f0cfd06adeab4bcdaf0d4501fe97d507eb449219
Alexander Ursu <alexander.ursu@gmail.com>
2021-06-02 10:37:08 +0200

    Corrected path too BinTray repo

--- manifests/repo/bintray.pp
+++ manifests/repo/bintray.pp
@@ -16,7 +16,7 @@ class lsys::repo::bintray (
 
   if $facts['os']['name'] in ['RedHat', 'CentOS'] and $facts['os']['release']['major'] in ['6', '7', '8'] {
     yumrepo { 'bintray-custom':
-      baseurl       => 'https://dl.bintray.com/aursu/custom/centos/$releasever/',
+      baseurl       => 'https://rpmb.jfrog.io/artifactory/custom/centos/$releasever/',
       descr         => 'PHP dependencies',
       enabled       => '0',
       gpgcheck      => '0',
@@ -28,7 +28,7 @@ class lsys::repo::bintray (
 
     if $php5_enable {
       yumrepo { 'bintray-php5custom':
-        baseurl       => 'https://dl.bintray.com/aursu/php5custom/centos/$releasever/',
+        baseurl       => 'https://rpmb.jfrog.io/artifactory/php5custom/centos/$releasever/',
         descr         => 'PHP 5.6 packages and extensions',
         enabled       => '0',
         gpgcheck      => '0',
@@ -41,7 +41,7 @@ class lsys::repo::bintray (
 
     if $php71_enable {
       yumrepo { 'bintray-php71custom':
-        baseurl       => 'https://dl.bintray.com/aursu/php71custom/centos/$releasever/',
+        baseurl       => 'https://rpmb.jfrog.io/artifactory/php71custom/centos/$releasever/',
         descr         => 'PHP 7.1 packages and extensions',
         enabled       => '0',
         gpgcheck      => '0',
@@ -54,7 +54,7 @@ class lsys::repo::bintray (
 
     if $php73_enable {
       yumrepo { 'bintray-php73custom':
-        baseurl       => 'https://dl.bintray.com/aursu/php73custom/centos/$releasever/',
+        baseurl       => 'https://rpmb.jfrog.io/artifactory/php73custom/centos/$releasever/',
         descr         => 'PHP 7.3 packages and extensions',
         enabled       => '0',
         gpgcheck      => '0',
@@ -67,7 +67,7 @@ class lsys::repo::bintray (
 
     if $php74_enable {
       yumrepo { 'bintray-php74custom':
-        baseurl       => 'https://dl.bintray.com/aursu/php74custom/centos/$releasever/',
+        baseurl       => 'https://rpmb.jfrog.io/artifactory/php74custom/centos/$releasever/',
         descr         => 'PHP 7.4 packages and extensions',
         enabled       => '0',
         gpgcheck      => '0',
@@ -80,7 +80,7 @@ class lsys::repo::bintray (
 
     if $php8_enable {
       yumrepo { 'bintray-php8custom':
-        baseurl       => 'https://dl.bintray.com/aursu/php8custom/centos/$releasever/',
+        baseurl       => 'https://rpmb.jfrog.io/artifactory/php8custom/centos/$releasever/',
         descr         => 'PHP 8.x packages and extensions',
         enabled       => '0',
         gpgcheck      => '0',
