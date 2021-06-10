817fa6c60cda680a2b3a224ee518163e41b10b9e
Alexander Ursu <alexander.ursu@gmail.com>
2021-06-11 01:10:38 +0200

    Added rsyslog upstream repo

---
+++ manifests/repo/rsyslog.pp
@@ -0,0 +1,42 @@
+# @summary Rsyslog upstream repo
+#
+# Rsyslog upstream repo
+# https://www.rsyslog.com/rhelcentos-rpms/
+#
+# @example
+#   include lsys::repo::rsyslog
+class lsys::repo::rsyslog {
+  case $facts['os']['name'] {
+    'RedHat', 'CentOS': {
+      yumrepo { 'rsyslog_v8':
+        ensure    => 'present',
+        descr     => 'Adiscon CentOS-$releasever - local packages for $basearch',
+        baseurl   => 'http://rpms.adiscon.com/v8-stable/epel-$releasever/$basearch',
+        enabled   => '0',
+        gpgcheck  => '1',
+        gpgkey    => 'http://rpms.adiscon.com/RPM-GPG-KEY-Adiscon',
+        sslverify => '0',
+        notify    => Class['lsys::repo'],
+      }
+
+      file { '/etc/yum.repos.d/rsyslog_v8.repo':
+        mode => '0640',
+      }
+
+      $key_package_name = 'gpg-pubkey-e00b8985-512dde96'
+      $rsyslog_gpg_key = '/etc/pki/rpm-gpg/RPM-GPG-KEY-Adiscon'
+
+      file { $rsyslog_gpg_key:
+        content => file('lsys/repo/RPM-GPG-KEY-Adiscon'),
+      }
+
+      exec { "rpm --import ${rsyslog_gpg_key}":
+        path    => '/bin:/usr/bin',
+        unless  => "rpm -q ${key_package_name}",
+        onlyif  => "test -f ${rsyslog_gpg_key}",
+        require => File[$rsyslog_gpg_key],
+      }
+    }
+    default: {}
+  }
+}
