67cc34248c12ff3ebcca9c25c93d57c0056b2e29
Alexander Ursu <alexander.ursu@gmail.com>
2021-12-17 14:41:07 +0100

    Added ability to install package out of corporate repo

--- manifests/tools/package.pp
+++ manifests/tools/package.pp
@@ -2,20 +2,43 @@
 #
 # Install package if ensure is set
 #
+# @param corporate_repo
+#   if specified - use only this repo for installation of the package
+#
 # @example
 #   lsys::tools::package { 'sudo': }
 define lsys::tools::package (
+  String  $package_name   = $name,
   Optional[Lsys::PackageVersion]
-          $ensure = undef,
+          $ensure         = undef,
+  Optional[String]
+          $corporate_repo = undef,
 ) {
+  if  $facts['os']['name'] in ['RedHat', 'CentOS'] and $corporate_repo {
+    if $facts['os']['release']['major'] == '8' {
+      $package_provider = 'dnf'
+      $package_install_options  = ['--repo', $corporate_repo]
+    }
+    else {
+      $package_provider = 'yum'
+      $package_install_options  = ['--disablerepo', '*', '--enablerepo', $corporate_repo]
+    }
+  }
+  else {
+    $package_provider = undef
+    $package_install_options = []
+  }
+
   if $ensure {
     $package_ensure = $ensure ? {
       String  => $ensure,
       default => 'installed',
     }
 
-    package { $name:
-      ensure => $package_ensure,
+    package { $package_name:
+      ensure          => $package_ensure,
+      provider        => $package_provider,
+      install_options => $package_install_options,
     }
   }
 }
