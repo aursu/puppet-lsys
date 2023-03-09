1826e19fe749d50e5fa5b1de28e640cbe01f219d
Alexander Ursu <alexander.ursu@gmail.com>
2023-03-09 16:42:47 +0100

    Added permissions set for runtime directories

--- manifests/postfix/client.pp
+++ manifests/postfix/client.pp
@@ -6,10 +6,16 @@
 #  The next-hop destination(s) for non-local mail; overrides non-local domains in recipient addresses
 #  http://www.postfix.org/postconf.5.html#relayhost
 #
+# @param postdrop_nosgid
+#   Whether to set ip proper permissions for Postfix runtime directories in
+#   case if SGID removed from /usr/sbin/postdrop and /usr/sbin/postqueue
+#   (e.g. in scope of security hardening)
+#
 # @example
 #   include lsys::postfix::client
 class lsys::postfix::client (
   Optional[Variant[Stdlib::Fqdn, Stdlib::IP::Address]] $relayhost = undef,
+  Boolean $postdrop_nosgid = false,
 ) {
   if $relayhost {
     $enable_mta = true
@@ -24,7 +30,6 @@ class lsys::postfix::client (
     mta            => $enable_mta,
     relayhost      => $relayhost,
   }
-
   contain postfix
 
   case $facts['os']['family'] {
@@ -42,6 +47,27 @@ class lsys::postfix::client (
         home       => '/var/spool/postfix',
         managehome => false,
         shell      => '/sbin/nologin',
+      }
+      # postdrop:x:90:
+      group { 'postdrop':
+        ensure => present,
+        gid    => 90,
+      }
+      if $postdrop_nosgid {
+        file { '/var/spool/postfix/maildrop':
+          ensure  => directory,
+          group   => 'postdrop',
+          owner   => 'postfix',
+          mode    => '1733',
+          require => Class['postfix'],
+        }
+        file { '/var/spool/postfix/public':
+          ensure  => directory,
+          group   => 'postdrop',
+          owner   => 'postfix',
+          mode    => '0711',
+          require => Class['postfix'],
+        }
       }
     }
     default: {}
--- spec/classes/postfix/client_spec.rb
+++ spec/classes/postfix/client_spec.rb
@@ -8,6 +8,40 @@ describe 'lsys::postfix::client' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_user('postfix')
+      }
+
+      it {
+        is_expected.to contain_group('postdrop')
+      }
+
+      it {
+        is_expected.not_to contain_file('/var/spool/postfix/maildrop')
+      }
+
+      it {
+        is_expected.not_to contain_file('/var/spool/postfix/public')
+      }
+
+      context 'when SGID bits removed from binaries' do
+        let(:params) do
+          {
+            postdrop_nosgid: true,
+          }
+        end
+
+        it {
+          is_expected.not_to contain_file('/var/spool/postfix/maildrop')
+            .with_mode('1733')
+        }
+  
+        it {
+          is_expected.not_to contain_file('/var/spool/postfix/public')
+          .with_mode('0711')
+        }
+      end
     end
   end
 end
