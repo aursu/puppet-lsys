8ee617d4a9f0af453cae4a694fb498848d41a071
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-10-17 21:31:11 +0200

    Added testing assets

---
+++ tests/bolt/Puppetfile
@@ -0,0 +1,5 @@
+# This Puppetfile is managed by Bolt. Do not edit.
+# For more information, see https://pup.pt/bolt-modules
+
+# The following directive installs modules to the managed moduledir.
+moduledir '.modules'
---
+++ tests/bolt/plans/server.pp
@@ -0,0 +1,11 @@
+plan postgres_bootstrap::server (
+  TargetSpec $targets = 'puppetservers',
+) {
+  run_task(bsys::install_yum, $targets)
+  run_task(puppet_agent::install, $targets, stop_service => true)
+
+  run_plan(facts, $targets)
+  return apply($targets) {
+    class { 'lsys': }
+  }
+}
