ed2ca06cd3f786a1d059d71608e2f8d0740331a1
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-10-18 18:46:56 +0200

    Checked if Postfix  template compatible with RL9

--- tests/bolt/plans/server.pp
+++ tests/bolt/plans/server.pp
@@ -10,7 +10,7 @@ plan lsys_tests::postfix (
       relayhost          => 'mail.domain.tld',
       postdrop_nosgid    => true,
       maillog_file       => '/var/log/maillog',
-      master_os_template => 'lsys/postfix/master.cf.rocky-8.erb',
+      master_os_template => 'lsys/postfix/master.cf.rocky.erb',
     }
   }
 }
